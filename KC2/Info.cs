216a8b28d0ddc27dc78280b166dd034f4eeda161
nathanroyhawes <nathanhawes@proton.me>
2023-03-09 19:07:39 -0500

    first commit

---
+++ KC2/Info.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KC2
+{
+    public class Info : VideoGame
+    {
+        public string Genre { get; set; }
+    }
+}
---
+++ KC2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace KC2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many games do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+            var recordList = new List<Info>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var info = new Info();
+
+                Console.WriteLine("What is name of the game?");
+                info.Name = Console.ReadLine();
+
+                Console.WriteLine("What is the genre?");
+                info.Genre = Console.ReadLine();
+
+                recordList.Add(info);
+            }
+
+            Console.WriteLine();
+            // Print out the list of records using Console.WriteLine()
+
+        }
+    }
+}
